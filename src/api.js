8effbe3e753b5c273e87df341b3e609341d0c702
adel essam elfar <adel.essam.elfar@gmail.com>
2022-06-24 21:03:17 +0200

    callvito task

--- src/api.js
+++
@@ -1,9 +0,0 @@
-const API_BASE_ADDRESS = 'http://localhost:8080';
-
-    export default function getUsers() {
-       const uri = API_BASE_ADDRESS + "/api/tasks";
-       
-       return fetch(uri, {
-           method: 'GET'
-       });
-   }
--- src/components/Tabel.js
+++ src/components/Tabel.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -8,24 +8,20 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import { useSelector, useDispatch } from "react-redux";
-import {  TextField } from "@mui/material";
-import { editTask,delTask, getTasks } from "../redux/actions/tasks";
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import CheckIcon from '@mui/icons-material/Check';
-import ClearIcon from '@mui/icons-material/Clear';
+import { TextField } from "@mui/material";
+import { editTask, delTask, getTasks } from "../redux/actions/tasks";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CheckIcon from "@mui/icons-material/Check";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650
-  }
+    minWidth: 650,
+  },
 });
 
-
- 
 export default function Tabel() {
-
-
   const dispatch = useDispatch();
   const [title, setTitle] = useState("");
   const [copy, setCopy] = useState({
@@ -37,7 +33,6 @@ export default function Tabel() {
   const taskState = useSelector((state) => state.handleTasks);
   const [loadingData, setLoadingData] = useState(true);
 
-  
   const [rows, setRows] = useState([]);
   const [enabled, setEnabled] = useState("");
 
@@ -51,81 +46,127 @@ export default function Tabel() {
   };
 
   const handleEdit = (row) => {
-    setEnabled("")
-    setLoadingData(true)
-    setTitle(row.title)
-   dispatch(editTask(row.id, row.title, row.description));
+    setEnabled("");
+    setLoadingData(true);
+    setTitle(row.title);
+    dispatch(editTask(row.id, row.title, row.description));
   };
 
   const handleDel = (id) => {
-    setLoadingData(true)
+    setLoadingData(true);
     dispatch(delTask(id));
   };
 
-  const isDisabled = (id)=>{
-    return enabled!==id
-  }
-  const clear = ()=>{
-    
-    setEnabled("")
-    
-  }
+  const isDisabled = (id) => {
+    return enabled !== id;
+  };
+  const clear = () => {
+    setEnabled("");
+  };
 
+  //when the app loads , we laod the data
   useEffect(() => {
     async function getData() {
-dispatch(getTasks())
+      dispatch(getTasks());
     }
     if (loadingData) {
-      // if the result is not ready so you make the axios call
       getData();
     }
   }, []);
 
+  //when any change happens , we get the data from the redux state
   useEffect(() => {
-    Promise.resolve(taskState).then(function(value) {
-      setLoadingData(true)
-      setRows(value.data)
-    }, function(value) {
-    });
+    Promise.resolve(taskState).then(
+      function (value) {
+        setLoadingData(true);
+        setRows(value.data);
+      },
+      function (value) {}
+    );
   }, [taskState]);
 
   return (
     <>
-      <Paper >
-        <TableContainer style={{marginBottom:"25px"}} >
+      <Paper>
+        <TableContainer style={{ marginBottom: "25px" }}>
           <Table className={classes.table} aria-label="simple table">
             <TableHead>
               <TableRow>
-
                 <TableCell align="right">id</TableCell>
-                <TableCell align="center"  >title</TableCell>
-                <TableCell align="center"  >description</TableCell>
-                <TableCell  align="right" >action</TableCell>
-
+                <TableCell align="center">title</TableCell>
+                <TableCell align="center">description</TableCell>
+                <TableCell align="right">action</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {rows.map((row) => (
                 <TableRow key={row.name}>
-             
                   <TableCell align="right">{row.id}</TableCell>
-                  
-                  <TableCell align="right"><TextField  style ={{width: '100%'}}  variant="standard"  onChange={(e)=>{handleTitle(e);row.title=e.target.value }} value={row.title} disabled={isDisabled(row.id)}/>  </TableCell>
-                  <TableCell align="right"><TextField  style ={{width: '100%'}}  variant="standard" onChange={(e)=>{handleDescription(e);row.description=e.target.value }} value={row.description} disabled={isDisabled(row.id)}/>  </TableCell>
-                  <TableCell align="right">< DeleteIcon  hidden={ enabled  }  onClick={()=>{handleDel(row.id)}} /><EditIcon hidden={ enabled  }  onClick={()=>{setEnabled(row.id);
-                    setCopy(previousState => {
-                    return { ...previousState, title: row.title,description:row.description }
-                  });
-                }}  /> <CheckIcon onClick={()=>handleEdit(row) } hidden={ enabled!==row.id  }  />  <ClearIcon onClick={(e)=>{clear(e);row.title=copy.title;row.description=copy.description}} hidden={ enabled!==row.id  }  />    </TableCell>
+
+                  <TableCell align="right">
+                    <TextField
+                      style={{ width: "100%" }}
+                      variant="standard"
+                      onChange={(e) => {
+                        handleTitle(e);
+                        row.title = e.target.value;
+                      }}
+                      value={row.title}
+                      disabled={isDisabled(row.id)}
+                    />{" "}
+                  </TableCell>
+                  <TableCell align="right">
+                    <TextField
+                      style={{ width: "100%" }}
+                      variant="standard"
+                      onChange={(e) => {
+                        handleDescription(e);
+                        row.description = e.target.value;
+                      }}
+                      value={row.description}
+                      disabled={isDisabled(row.id)}
+                    />{" "}
+                  </TableCell>
+                  <TableCell align="right">
+                    <DeleteIcon
+                      hidden={enabled}
+                      onClick={() => {
+                        handleDel(row.id);
+                      }}
+                    />
+                    <EditIcon
+                      hidden={enabled}
+                      onClick={() => {
+                        setEnabled(row.id);
+                        setCopy((previousState) => {
+                          return {
+                            ...previousState,
+                            title: row.title,
+                            description: row.description,
+                          };
+                        });
+                      }}
+                    />{" "}
+                    <CheckIcon
+                      onClick={() => handleEdit(row)}
+                      hidden={enabled !== row.id}
+                    />{" "}
+                    <ClearIcon
+                      onClick={(e) => {
+                        clear(e);
+                        row.title = copy.title;
+                        row.description = copy.description;
+                      }}
+                      hidden={enabled !== row.id}
+                    />{" "}
+                  </TableCell>
                 </TableRow>
-                
               ))}
             </TableBody>
           </Table>
         </TableContainer>
       </Paper>
       <br />
-
     </>
   );
 }
